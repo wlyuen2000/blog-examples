9b1d053a3b7a791fc1ebf4ab8b8272886e1f962c
Wang Yuen <wangdemo2018@gmail.com>
2025-06-26 17:18:38 -0400

    test

--- simple-vpc-module/example/subnets.tf
+++ simple-vpc-module/example/subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "public_subnets" {
 
   count = length(var.public_subnet_cidrs)
 
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.this.id
 
   cidr_block = element(var.public_subnet_cidrs, count.index)
 
@@ -22,7 +22,7 @@ resource "aws_subnet" "private_subnets" {
 
   count = length(var.private_subnet_cidrs)
 
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.this.id
 
   cidr_block = element(var.private_subnet_cidrs, count.index)
 
